78b09662c2c20eb24ac38eed50cac9e707a7bd6a
emysa341 <ammad.emy36@gmail.com>
2019-04-26 19:20:35 +0200

    Add project files.

---
+++ MyAweSomeLibrary/SomeAdditionalWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MyAweSomeLibrary
+{
+    public class SomeAdditionalWork
+    {
+        public  async Task<string> SomeActionAsync()
+        {
+            await Task.Delay(10);
+
+            return "WorkDone";
+        }
+
+        public void Dispose()
+        {
+            Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ MyAweSomeLibrary/SomeMoreWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MyAweSomeLibrary
+{
+    public class SomeWork : IDisposable
+    {
+        private SomeAdditionalWork _someAdditionalWork;
+
+        public SomeWork(SomeAdditionalWork someAdditionalWork)
+        {
+            _someAdditionalWork = someAdditionalWork;
+        }
+
+        public async Task SomeAction()
+        {
+            await _someAdditionalWork.SomeActionAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
